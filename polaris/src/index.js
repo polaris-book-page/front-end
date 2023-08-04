29fa7a7ae1edc2677c9b21c07c6e0d98ef0cf7e9
kjh073 <9279482@naver.com>
2023-08-05 04:33:27 +0900

    [design] add floating label css file & apply to index.js - add floaing label css file - css file apply to index.js

--- polaris/src/index.js
+++ polaris/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/fonts/font.css";
+import "./float-label.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
