095df8bf0bc593e1edc16f5910f5f4542a352c5e
otcroz <dbtndus0527@gmail.com>
2023-09-14 00:01:09 +0900

    [feat] Setting calendar Lib in CalendarPage - Setting calendar Lib in CalendarPage

--- polaris/src/page/mypage/CalendarPage.js
+++ polaris/src/page/mypage/CalendarPage.js
@@ -1,5 +1,24 @@
-const CalendarPage = () =>{
-    return (<>CalendarPage</>)
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+
+const CalendarPage = () => {
+
+    // react-query
+
+    // save mark book: start date, end date
+     const [mark, setMark] = useState([]);
+
+    return (
+        <>
+            <Calendar
+                minDetail="month"
+                maxDetail="month"
+                navigationLabel={null}
+                showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
+                className="mx-auto w-full text-sm border-b"
+                />
+        </>
+    )
 }
 
 export default CalendarPage;
