cfb26a5ae5daea4389aefc8ca3cf34b6013460cf
kjh073 <9279482@naver.com>
2023-09-15 00:56:07 +0900

    [fix] Fix late pageation changes when changing categories - fix late pageation changes when changing categories

--- polaris/src/page/search/SearchResultPage.js
+++ polaris/src/page/search/SearchResultPage.js
@@ -21,7 +21,6 @@ const SearchResultPage = () => {
     const maxResults = 50
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [isActive1, setIsActive1] = useState(false);
     const [isActive2, setIsActive2] = useState(false);
     const [isActive3, setIsActive3] = useState(false);
     const [categories, setCategories] = useState([]);
@@ -32,8 +31,7 @@ const SearchResultPage = () => {
         setLoading(true);
         try {
             // const result = await axios.get(query);
-            console.log("before api: ", categoryOptions[categories])
-            const result = await axios.get(`ttb/api/ItemSearch.aspx?ttbkey=${process.env.REACT_APP_TTBKEY}&Query=물고기
+            const result = await axios.get(`ttb/api/ItemSearch.aspx?ttbkey=${process.env.REACT_APP_TTBKEY}&Query=모순
             &Cover=Big&QueryType=Title&MaxResults=${maxResults}&Sort=${orderOptionsSelect[orderOptions.indexOf(order)]}&start=${currentArray + 1}&SearchTarget=${bookOptionsSelect[bookOptions.indexOf(bookType)]}&CategoryId=${categoryOptions[categories]}&output=js&Version=20131101`);
             console.log(result.data)
             setData(result.data);
@@ -43,6 +41,7 @@ const SearchResultPage = () => {
                 newItemSlice.push(result.data.item.slice(i, i + pagePerLimit));
             }
             setItemsPerPage(newItemSlice)
+
         } catch (e) {
             console.log(e);
         }
@@ -60,17 +59,9 @@ const SearchResultPage = () => {
         // if (bookOptionsSelect[bookOptions.indexOf(bookType)] !== bookOptionsSelect[0]){
         //     query += `&SearchTarget=${bookOptionsSelect[bookOptions.indexOf(bookType)]}`
         // }
-        // if (bookType === '종이책') {
-
-        // } else if (bookType === '전자책'){
-
-        // } else {
-
-        // }
         // if (categoryOptions[categories] !== 0){
         //     query += `&CategoryId=${categoryOptions[categories]}`
         // }
-        console.log("cate: ", categories)
         searchResultFunc();
         if (prevBookTypeRef.current !== bookType || prevOrderRef.current !== order) {
             setcurrentPage(1);
@@ -104,7 +95,7 @@ const SearchResultPage = () => {
             }
             setSlicePages(newSlicePages);
         }
-    }, [data]);
+    }, [data, categories]);
 
     if (!(data && currentItems)) {
         return null;
@@ -115,27 +106,14 @@ const SearchResultPage = () => {
         setcurrentPage(newCurrentPage);
     };
 
-    const toggleActive1 = () => {
-        setIsActive1(prevIsActive => !prevIsActive);
-        setIsActive2(false);
-        setIsActive3(false);
-    };
-
     const toggleActive2 = () => {
-        setIsActive1(false);
         setIsActive2(prevIsActive => !prevIsActive);
         setIsActive3(false);
     };
 
     const toggleActive3 = () => {
-        setIsActive1(false);
         setIsActive2(false);
         setIsActive3(prevIsActive => !prevIsActive);
-    };
-
-    const handleBookTypeChange = (bookType) => {
-        setBookType(bookType);
-        console.log(bookOptionsSelect[bookOptions.indexOf(bookType)])
     };
     
     const handleOrderChange = (order) => {
@@ -150,37 +128,36 @@ const SearchResultPage = () => {
 
     return (
         <>
-                <MainContainer className="container">
-                    <SearchBox>
-                        <SearchInput placeholder="책 이름을 입력해주세요."/>
-                        <SearchBtn size="54"/>
-                    </SearchBox>
-                    <ResultText>'{data.query}'에 대한 검색 결과({data.totalResults})</ResultText>
-                    <FilterContainer>
-                        <FilterDropdown 
-                            isActive={isActive3}
-                            setIsActive={toggleActive3}
-                            options={categoryKeys}
-                            setOptions={handleCategoryChange}
+            <MainContainer className="container">
+                <SearchBox>
+                    <SearchInput placeholder="책 이름을 입력해주세요."/>
+                    <SearchBtn size="54"/>
+                </SearchBox>
+                <ResultText>'{data.query}'에 대한 검색 결과({data.totalResults})</ResultText>
+                <FilterContainer>
+                    <FilterDropdown 
+                        isActive={isActive3}
+                        setIsActive={toggleActive3}
+                        options={categoryKeys}
+                        setOptions={handleCategoryChange}
                         />
-                        <FilterDropdown 
-                            isActive={isActive2}
-                            setIsActive={toggleActive2}
-                            options={orderOptions}
-                            setOptions={handleOrderChange}
-                            //가격순은 옵션에 없음 
+                    <FilterDropdown 
+                        isActive={isActive2}
+                        setIsActive={toggleActive2}
+                        options={orderOptions}
+                        setOptions={handleOrderChange}
                         />
-                    </FilterContainer>
-                    {currentItems && currentItems.map((item, index) => (
-                        <GridBox key={index} item={item} gridArea={`gridBox${index % 10 + 1}`} />
-                    ))}
-                    <Pagination
-                        slicePages={slicePages}
-                        currentArray={currentArray}
-                        currentPage={currentPage}
-                        onPageChange={handlePageChange}
-                    />
-                </MainContainer>
+                </FilterContainer>
+                {currentItems && currentItems.map((item, index) => (
+                    <GridBox key={index} item={item} gridArea={`gridBox${index % 10 + 1}`} />
+                ))}
+                <Pagination
+                    slicePages={slicePages}
+                    currentArray={currentArray}
+                    currentPage={currentPage}
+                    onPageChange={handlePageChange}
+                />
+            </MainContainer>
             <FooterBar/>
         </>
     )
@@ -189,6 +166,8 @@ const SearchResultPage = () => {
 const FilterContainer = styled.div`
     grid-area: resultFilter;
     display: flex;
+    justify-self: end;
+    margin-right: 20px;
 `
 
 const MainContainer = styled.div`
@@ -237,7 +216,5 @@ const ResultText = styled.div`
     font-family: "KOTRA_BOLD";
     justify-self: start;
 `;
-
-
 
 export default SearchResultPage;
