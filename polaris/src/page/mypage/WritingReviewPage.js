bc2c39711a605a36a6f20c28a4ac22715887ab92
otcroz <dbtndus0527@gmail.com>
2023-09-22 03:51:15 +0900

    [design] Implement to WritingReviewPage design - Implement to WritingReviewPage design

--- polaris/src/page/mypage/WritingReviewPage.js
+++ polaris/src/page/mypage/WritingReviewPage.js
@@ -1,5 +1,233 @@
-const WritingReviewPage = () =>{
-    return (<>WritingReviewPage</>)
+import styled from "styled-components";
+import StarRating from "../../component/StarRating";
+
+const WritingReviewPage = () => {
+    return (
+        <>
+            <Background>
+                <Container>
+                    {/* book image */}
+                    <BookImageBox>
+                        <BookImage>
+                            <img style={{ width: 80, height: 130 }} />                            
+                        </BookImage>
+                        <TitleText color={'white'} size={'16px'}>책 제목</TitleText>
+                        <ContentText color={'white'} size={'13px'}>저자</ContentText>
+                        <StarRating rating={3.5} size={'20px'} />
+                    </BookImageBox>
+                    {/* input date */}
+                    <DateInputBox>
+                        <TitleText color={'white'} size={"16px"}>읽기 시작한 날짜</TitleText>
+                        <DateInput />
+                        <div style={{height: 20}} />
+                        <TitleText color={'white'} size={"16px"}>읽기 종료한 날짜</TitleText>
+                        <DateInput />
+                    </DateInputBox>
+                    {/* dashed line */}
+                    <Line />
+                    {/* input quote */}
+                    <QuoteContainer>
+                        <QuoteColumnAlignContainer>
+                            <QuoteInputTitleBox>
+                                <QuoteInputTitleText flex={0.8} color={'white'} size={'16px'}>마음에 남았던 구절</QuoteInputTitleText>
+                                <QuoteInputTitleText flex={0.2} color={'white'} size={'16px'}>페이지</QuoteInputTitleText>
+                            </QuoteInputTitleBox>
+                            <QuoteInputBox>
+                                <QuoteInput style={{flex: 0.8}} />
+                                <QuotePageInput style={{flex: 0.2}}/>
+                            </QuoteInputBox>
+                            <ContentText color={'white'} size={'12px'} style={{margin: 'auto', float:'right'}}>+구절 추가하기</ContentText>
+                    </QuoteColumnAlignContainer>
+                    </QuoteContainer>
+                    {/* input review */}
+                    <div style={{height: 20}} />
+                    <ReviewBox>
+                        <TitleText color='white' size='16px'>리뷰</TitleText>
+                        <ReviewInput />
+                    </ReviewBox>
+                    <div style={{height: 10}} />
+                    <Button>
+                        <ContentText color={'white'} size={'16px'}>내 행성에 추가하기</ContentText>
+                    </Button>
+                </Container>
+            </Background>
+        </>
+    )
 }
+
+// text
+const TitleText = styled.text`
+    color: ${(props) => props.color || 'gray'};
+    font-family: "KOTRA_BOLD";
+    font-size: ${(props) => props.size || '12px'};
+`;
+
+const ContentText = styled.text`
+    color: ${(props) => props.color || 'gray'};
+    font-family: "KOTRA_GOTHIC";
+    font-size: ${(props) => props.size || '12px'};
+`
+
+// container
+const Background = styled.div`
+    position: flex;
+    justify-content: center;
+    background-color: #4659a9;
+    padding: 20px;
+`;
+
+const Container = styled.div`
+    display: flex;
+    margin: auto;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.3);
+    border-radius: 50px;
+    flex-direction: column;
+    padding: 40px 20px;
+    height: 100vh;
+    width: 90vh;
+    box-shadow: 0px 2px 7px #00000022;
+
+    @media screen and (max-width: 900px) {
+        width: 60vh;
+    }
+
+    @media screen and (max-width: 500px) {  
+        width: 50vh;
+    }
+`;
+
+const QuoteContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const QuoteColumnAlignContainer = styled.div`
+    flex-direction: column;
+    flex: 1;
+    align-items: center;
+`;
+
+// box
+const BookImageBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const DateInputBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 30px 0;
+`;
+
+const QuoteInputTitleBox = styled.div`
+    display:flex;
+    flex-direction: row;
+    flex: 1;
+    justify-content: center;
+`;
+
+const QuoteInputTitleText = styled.div`
+    flex: ${(props) => props.flex || 1};
+    font-family: "KOTRA_BOLD";
+    font-size: ${(props) => props.size || '12px'};
+    color: ${(props) => props.color || 'grey'};
+    text-align: center;
+`;
+
+const QuoteInputBox = styled.div`
+    display: flex;
+    flex: 1;
+`;
+
+const ReviewBox = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+`;
+
+// component
+const BookImage = styled.div`
+    background-color: #4659a9;
+    border-radius: 10px;
+    padding: 10px;
+    margin-bottom: 10px;
+`;
+
+const DateInput = styled.input`
+    font-size: 14px;
+    flex: 1;
+    color: white;
+    border-bottom: solid;
+    border-color: white;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    background: transparent;
+    padding: 4px;
+    text-align: center;
+    font-family: "KOTRA_BOLD";
+    
+`;
+
+const QuoteInput = styled.input`
+    width: 40vh;
+    border: none;
+    border-radius: 50px;
+    background-color: white;
+    padding: 4px;
+    font-family: "KOTRA_GOTHIC";
+    margin: 3px;
+`;
+
+const QuotePageInput = styled.input`
+    width: 45px;
+    border: none;
+    border-radius: 50px;
+    background-color: white;
+    padding: 4px;
+    font-family: "KOTRA_GOTHIC";
+    text-align: center;
+    margin: 3px;
+`;
+
+const ReviewInput = styled.textarea`
+    width: 450px;
+    font-size: 14px;
+    flex: 1;
+    border: none;
+    border-radius: 20px;
+    background-color: white;
+    padding: 10px;
+    font-family: "KOTRA_GOTHIC";
+`;
+
+const Line = styled.div`
+    border-color: white;
+    border-width: 2px;
+    border-style: dashed;
+    width: 100%;
+    margin-bottom: 40px;
+    margin-top: 40px;
+
+`;
+
+const Button = styled.div`
+    width: 45%;
+    font-family: "KOTRA_BOLD";
+    color: white;
+    background-color: #4659A9;
+    border-style: none;
+    font-size: 16px;
+    padding: 7px 50px;
+    margin: 10px;
+    border-radius: 50px;
+    text-align: center;
+`;
+
 
 export default WritingReviewPage;
