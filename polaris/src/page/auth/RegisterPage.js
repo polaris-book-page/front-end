05c61d736ddd4c0999ae11f69b3914f09836d7aa
kjh073 <9279482@naver.com>
2023-10-01 19:41:09 +0900

    [fix] Connect client-sever & add valid check function - connect client-sever - work duplicate check function - work valid check function

--- polaris/src/page/auth/RegisterPage.js
+++ polaris/src/page/auth/RegisterPage.js
@@ -1,21 +1,172 @@
 import styled from "styled-components";
 import React, { useState } from 'react';
-import NavBar from "../../component/NavBar";
 import FooterBar from "../../component/FooterBar";
 import Checkbox from "../../component/CheckBox";
-
+import axios from "axios";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useQueryClient } from '@tanstack/react-query'
 
 const RegisterPage = () =>{
     const [agreePrivacyPolicy, setAgreePrivacyPolicy] = useState(false);
-    // const [userId, setUserId] = useState<String>("")
-	// const [Password, setPassword] = useState<String>("")
-	// const [ConfirmPassword, setConfirmPassword] = useState<String>("")
-	// const [nickname, setNickname] = useState<String>("")
-    // const [Email, setEmail] = useState<String>("")
+    const [_id, setUserId] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirmPassword, setConfirmPassword] = useState('')
+	const [nickname, setNickname] = useState('')
+    const [email, setEmail] = useState('')
+    // set invalid message
+    const [_idMsg, setUserIdMsg] = useState(' dkadsas')
+	const [passwordMsg, setPasswordMsg] = useState(' ')
+	const [confirmPasswordMsg, setConfirmPasswordMsg] = useState(' ')
+	const [nicknameMsg, setNicknameMsg] = useState(' ')
+    const [emailMsg, setEmailMsg] = useState(' ')
+    // valid check
+    const [is_id, setIsUserId] = useState(false);
+	const [isPassword, setIsPassword] = useState(false);
+	const [isConfirmPassword, setIsConfirmPassword] = useState(false);
+	const [isNickname, setIsNickname] = useState(false);
+    const [isEmail, setIsEmail] = useState(false);
+    const queryClient = useQueryClient()
+    let navigate = useNavigate();
+
+    const { mutate } = useMutation({
+        mutationFn: async (userInfo) => {
+            const { data } = await axios.post(`http://localhost:3001/user/join`, userInfo, { withCredentials: true })
+            return data;
+        }, 
+        onSuccess: () => {
+            console.log("register success")
+            goHome()
+        },
+        onError: () => {
+            console.log("register failure")
+        }
+    });
+
+    const { idCheckQuery } = useQuery({
+        queryKey:  ['idCheck', _id],
+        queryFn: async () => {
+            const response = await fetch(
+                `http://localhost:3001/user/join/id-check/${_id}`,
+                );
+            const idCheckQuery = await response.json();
+            return idCheckQuery;
+        }, 
+        onSuccess: () => {
+            console.log("id-check success")
+            if (idCheckQuery && idCheckQuery.isAvailable) {
+                alert('사용 가능한 아이디입니다.')
+            }
+        },
+            enabled: is_id,
+    });
+    
+    const { nicknameCheckQuey } = useQuery({
+        queryKey:  ['nicknameCheck', nickname],
+        queryFn: async () => {
+            const response = await fetch(
+                `http://localhost:3001/user/join/id-check/${nickname}`,
+                );
+            const nicknameCheckQuey = await response.json();
+            return nicknameCheckQuey;
+        }, 
+        onSuccess: () => {
+            console.log("nickname-check success")
+            if (nicknameCheckQuey && nicknameCheckQuey.isAvailable) {
+                alert('사용 가능한 닉네임입니다.')
+            }
+        },
+            enabled: isNickname,
+    });
+    
+    
+    const handleDuplicateCheckId = () => {
+        console.log("chck in id")
+        queryClient.invalidateQueries(['idCheck', _id]);
+    };
+
+    const handleDuplicateCheckNickname = () => {
+        console.log("chck in nick")
+        queryClient.invalidateQueries(['nicknameCheck', nickname]);
+    };
+
+    let goHome = () => {
+        navigate('/');
+    };
+
+    const handleRegister = () => {
+        console.log(is_id, isPassword, isConfirmPassword, isNickname, isEmail, agreePrivacyPolicy)
+        if (is_id && isPassword && isConfirmPassword && isNickname && isEmail && agreePrivacyPolicy) {
+            mutate({ _id, password, nickname, email });
+        }
+    }
     
     const handleCheckboxChange = () => {
         setAgreePrivacyPolicy(!agreePrivacyPolicy);
     };
+
+    const onChangeId = (e) => {
+        const currentId = e.target.value;
+        setUserId(currentId);
+        console.log(currentId)
+        if (currentId.length < 2 || currentId.length > 20) {
+            setUserIdMsg('아이디는 2자리부터 20자리까지 가능합니다.')
+            setIsUserId(false)
+        } else {
+            setUserIdMsg('사용가능한 아이디입니다.')
+            setIsUserId(true)
+        }
+    }
+    const passwordReg = new RegExp('(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-])')
+    const onChangePassword = (e) => {
+        const currentPwd = e.target.value;
+        setPassword(currentPwd);
+        if (currentPwd.length < 6 || currentPwd.length > 16) {
+            setPasswordMsg('비밀번호는 6글자 이상, 16글자이하로 가능합니다.')
+            setIsPassword(false)
+        } else if (!passwordReg.test(currentPwd)) {
+            setPasswordMsg('비밀번호는 영어, 숫자, 특수문자 조합으로 구성되어야합니다.')
+            setIsPassword(false)
+        } else {
+            setPasswordMsg('사용가능한 비밀번호입니다.')
+            setIsPassword(true)
+        }
+    }
+    const onChangeConfirmPassword = (e) => {
+        const currentConfirmPwd = e.target.value;
+        setConfirmPassword(currentConfirmPwd);
+        if (currentConfirmPwd !== password) {
+            setConfirmPasswordMsg('입력하신 비밀번호와 일치하지 않습니다.')
+            setIsConfirmPassword(false)
+        } else {
+            setConfirmPasswordMsg('입력하신 비밀번호와 일치합니다.')
+            setIsConfirmPassword(true)
+        }
+    }
+    const onChangeNickname = (e) => {
+        const currentNickname = e.target.value;
+        setNickname(currentNickname);
+        if (currentNickname.length < 6 || currentNickname.length > 30) {
+            setNicknameMsg('닉네임은 6자리부터 30자리까지 가능합니다.')
+            setIsNickname(false)
+        } else {
+            setNicknameMsg('사용가능한 닉네임입니다.')
+            setIsNickname(true)
+        }
+    }
+    const emailReg = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')
+    const onChangeEmail = (e) => {
+        const currentEmail = e.target.value;
+        setEmail(currentEmail);
+        if (!emailReg.test(currentEmail)) {
+            setEmailMsg('올바른 이메일을 입력해주세요.')
+            setIsEmail(false)
+        } else {
+            setEmailMsg('사용가능한 이메일입니다.')
+            setIsEmail(true)
+        }
+    }
+
     return (
         <>
             <Background>
@@ -23,42 +174,49 @@ const RegisterPage = () =>{
                     <TitleText>REGISTER</TitleText>
                 <InputContainer>
                     <FlotingLabelContainer className="has-float-label">
-                        <FlotingLabelInput type="text" placeholder=""/>
+                        <FlotingLabelInput type="text" placeholder="" onChange={onChangeId}/>
                         <FlotingLabelTitle>아이디</FlotingLabelTitle>
                     </FlotingLabelContainer>
-                    <DuplicateCheckBtn>중복확인</DuplicateCheckBtn>
+                    <DuplicateCheckBtn onClick={handleDuplicateCheckId}>중복확인</DuplicateCheckBtn>
                 </InputContainer>
+                {_id.length > 0 && <ValidCheckMsg className={`message ${is_id ? 'success' : 'error'}`}>{_idMsg}</ValidCheckMsg>}
                 <InputContainer>
                     <FlotingLabelContainer className="has-float-label">
-                        <FlotingLabelInput type="password" placeholder=""/>
+                        <FlotingLabelInput type="password" placeholder="" onChange={onChangePassword}/>
                         <FlotingLabelTitle>비밀번호</FlotingLabelTitle>
-                        <ValidSentence>비밀번호는 영어, 숫자, 특수문자 중 2가지 조합으로 6글자 이상, 16글자 이하만 이용가능합니다.</ValidSentence>
+                        <ValidSentence>비밀번호는 영어, 숫자, 특수문자 조합으로 6글자 이상, 16글자 이하만 이용가능합니다.</ValidSentence>
                     </FlotingLabelContainer>
                 </InputContainer>
+                {password.length > 0 && <ValidCheckMsg className={`message ${isPassword ? 'success' : 'error'}`}>{passwordMsg}</ValidCheckMsg>}
                 <InputContainer>
                     <FlotingLabelContainer className="has-float-label">
-                        <FlotingLabelInput type="password" placeholder=""/>
+                        <FlotingLabelInput type="password" placeholder="" onChange={onChangeConfirmPassword}/>
                         <FlotingLabelTitle>비밀번호 확인</FlotingLabelTitle>
                     </FlotingLabelContainer>
                 </InputContainer>
+                {confirmPassword.length > 0 && <ValidCheckMsg className={`message ${isConfirmPassword ? 'success' : 'error'}`}>{confirmPasswordMsg}</ValidCheckMsg>}
                 <InputContainer>
                     <FlotingLabelContainer className="has-float-label">
-                        <FlotingLabelInput type="text" placeholder=""/>
+                        <FlotingLabelInput type="text" placeholder="" onChange={onChangeNickname}/>
                         <FlotingLabelTitle>닉네임</FlotingLabelTitle>
                     </FlotingLabelContainer>
-                    <DuplicateCheckBtn>중복확인</DuplicateCheckBtn>
+                    <DuplicateCheckBtn onClick={handleDuplicateCheckNickname}>중복확인</DuplicateCheckBtn>
                 </InputContainer>
-                <FlotingLabelContainer className="has-float-label">
-                    <FlotingLabelInput type="email" placeholder="polaris-book@gmail.com"/>
-                    <FlotingLabelTitle>이메일</FlotingLabelTitle>
-                </FlotingLabelContainer>
+                {nickname.length > 0 && <ValidCheckMsg className={`message ${isNickname ? 'success' : 'error'}`}>{nicknameMsg}</ValidCheckMsg>}
+                <InputContainer>
+                    <FlotingLabelContainer className="has-float-label">
+                        <FlotingLabelInput type="email" placeholder="polaris-book@gmail.com" onChange={onChangeEmail}/>
+                        <FlotingLabelTitle>이메일</FlotingLabelTitle>
+                    </FlotingLabelContainer>
+                </InputContainer>
+                {email.length > 0 && <ValidCheckMsg className={`message ${isEmail ? 'success' : 'error'}`}>{emailMsg}</ValidCheckMsg>}
                 <BtnContainer>
                     <Checkbox 
                         label={'개인정보처리방침에 동의합니다.'} 
                         selectedValue={agreePrivacyPolicy}
                         setSelectedValue={handleCheckboxChange}
                     />
-                    <NextBtn> 다음 </NextBtn>
+                    <NextBtn onClick={handleRegister}> 다음 </NextBtn>
                 </BtnContainer>
                 </RegisterContainer>
             </Background>
@@ -66,6 +224,21 @@ const RegisterPage = () =>{
         </>
     )
 }
+
+const ValidCheckMsg = styled.p`
+    &.message {
+        color: white;
+        font-family: "KOTRA_GOTHIC";
+        font-size: 13px;
+        margin: 0;
+        &.success {
+            color: white;
+        }
+        &.error {
+            color: #ff2727;
+        }
+    }
+`;
 
 const Background = styled.div`
     position: relative;
@@ -94,11 +267,11 @@ const TitleText = styled.h1`
 
 const InputContainer = styled.div`
     display: flex;
-    align-items: center;
+    /* align-items: center; */
 `;
 
 const FlotingLabelContainer = styled.label`
-    margin: 10px 0;
+    margin-top: 15px;
     flex-grow: 14;
 `;
 const FlotingLabelInput = styled.input`
@@ -127,6 +300,7 @@ const DuplicateCheckBtn = styled.button`
     color: white;
     flex-grow: 1;
     padding: 0;
+    padding-top: 10px;
     border-radius: 10px;
     border: 1.5px solid transparent;
     font-size: 15.5px;
