9abd293a29dd073c02945f5beeae7c12f4c1a079
kjh073 <9279482@naver.com>
2024-01-22 01:13:17 +0900

    [design] Implement Statistics page - implement Statistics page - input chart

---
+++ polaris/src/component/DrawChart1.js
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import * as helpers from 'chart.js/helpers';
+import LabelPluginProvider from './LabelPluginProvider';
+
+const DrawChart1 = ({ legendContainerId }) => {
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
+
+    const customLegendPlugin = {
+      afterDraw: function (chart) {
+        let ul = document.createElement('ul');
+        ul.style.listStyleType = 'none';
+        let color = chart.data.datasets[0].backgroundColor;
+
+        chart.data.labels.forEach(function (label, index) {
+          ul.innerHTML += `<li style="margin-bottom: 5px; display: inline-block; margin-right: 10px; color: ${color[index]};">
+            <span style="background-color: ${color[index]}; display: inline-block; width: 20px; height: 20px; border-radius: 50%; margin-right: 3px; border: 1.5px solid white"></span>
+            ${label}
+          </li>`;
+        });
+
+        let legendContainer = document.getElementById(`legend-container-${legendContainerId}`);
+        legendContainer.innerHTML = ul.outerHTML;
+      },
+    };
+
+    const ctx = chartRef.current.getContext('2d');
+    chartInstance.current = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: ['인문', '에세이', '소설', '과학', '철학'],
+        datasets: [{
+          label: ['책 타입'],
+          data: [12, 13, 3, 5, 2],
+          backgroundColor: [
+            '#2C2C60', '#4659A9', '#97A4E8', '#6F61C6', '#CBCDFA', '#D5CFFB'
+          ],
+          borderWidth: 1.5
+        }]
+      },
+      options: {
+        plugins: {
+          htmlLegend: {
+            containerID: `legend-container-${legendContainerId}`,
+          },
+          legend: {
+            display: false,
+          },
+          labels: {
+            labels: false
+          },
+          datalabels: {
+            formatter: function (value, ctx) {
+              var value = ctx.dataset.data[ctx.dataIndex];
+              return value > 0 ? Math.round(value / (ctx.dataset.data[0] + ctx.dataset.data[1] + ctx.dataset.data[2]) * 100) + ' %' : null;
+            },
+            font: {
+              size: '17px',
+            },
+            color: 'white',
+          },
+        }
+      },
+      plugins: [customLegendPlugin, ChartDataLabels],
+    });
+
+    return () => {
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    // <LabelPluginProvider>
+    <DrawChartContainer>
+      <div style={{ marginRight: '20px' }}>
+        <canvas ref={chartRef} id={`myChart-${legendContainerId}`} width="240" height="240"></canvas>
+      </div>
+      <div id={`legend-container-${legendContainerId}`} className="legend-div"></div>
+      <style>
+        {`
+          #legend-container-${legendContainerId} ul {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            padding: 0;
+            margin: 0;
+          }
+
+          #legend-container-${legendContainerId} ul li span {
+            vertical-align: middle;
+          }
+        `}
+      </style>
+    </DrawChartContainer>
+    // </LabelPluginProvider>
+  );
+};
+
+const DrawChartContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default DrawChart1;
--- polaris/src/page/mypage/StatisticsPage.js
+++ polaris/src/page/mypage/StatisticsPage.js
@@ -1,12 +1,192 @@
+import styled from "styled-components";
 import NavBar from "../../component/NavBar";
+import FooterBar from "../../component/FooterBar";
+import DrawChart1 from "../../component/DrawChart1";
+import DrawChart2 from "../../component/DrawChart2";
+// import Chartex from "../../component/Chartex"
 
 const StatisticsPage = () =>{
+
     return (
         <>
-            <NavBar/>
-            StatisticsPage
+        {/* <Background> */}
+            <NavBar />
+                <TitleContainer>
+                    <TitleText>나의 여행기록</TitleText>
+                </TitleContainer>
+                <StatisticsGrid className="container">
+                    <Rocket>
+                        <></>
+                        {/* <Chartex/> */}
+                    </Rocket>
+                    <Category>
+                        <SubtitleText1>읽은 카테고리</SubtitleText1>
+                            <Container1/>
+                            <DrawChartContainer>
+                                <DrawChart11 legendContainerId="1" />
+                            </DrawChartContainer>
+                    </Category>
+                    <Type>
+                        <SubtitleText2>책 타입</SubtitleText2>
+                        <Container2/>
+                        <DrawChartContainer>
+                            <DrawChart22 legendContainerId="2" />
+                        </DrawChartContainer>
+                    </Type>
+                    <Review>
+                        <SubtitleText3>지금까지 남긴 별점</SubtitleText3>
+                        <Container3></Container3>
+                    </Review>
+                    <Calendar>
+                        <SubtitleText4>월별 달력</SubtitleText4>
+                        <Container4></Container4>
+                    </Calendar>
+                </StatisticsGrid>
+            <FooterBar />
+            {/* </Background> */}
         </>
     )
 }
+
+// const Background = styled.div`
+//     background-color: pink;
+// `;
+
+const TitleContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 100px 0 30px 0;
+`;
+
+const TitleText = styled.p`
+    font-size: 50px;
+    margin: 0;
+    font-family: "KOTRA_BOLD";
+`;
+
+const StatisticsGrid = styled.div`
+    display: grid;
+    grid-template-areas:
+        "rocket category"
+        "rocket type"
+        "review calendar";
+`;
+
+const Rocket = styled.div`
+    grid-area: rocket;
+`;
+
+const Category = styled.div`
+    grid-area: category;
+    position: relative;
+`;
+
+const SubtitleText1 = styled.p`
+    text-align: center;
+    font-size: 30px;
+    font-family: "KOTRA_BOLD";
+    color: #4659A9;
+    position: absolute;
+    left: 195px;
+    bottom: 340px;
+`;
+
+const Container1 = styled.div`
+    width: 540px;
+    height: 323px;
+    border-radius: 61px;
+    background: #4659A9;
+    opacity: 0.3;
+    display: inline-block;
+    margin-bottom: 50px;
+`;
+
+const DrawChart11 = styled(DrawChart1)`
+`;
+
+const DrawChartContainer = styled.div`
+    position: absolute;
+    left: 110px;
+    bottom: 90px;
+`;
+
+const Type = styled.div`
+    grid-area: type;
+    position: relative;
+`;
+
+const SubtitleText2 = styled.p`
+    text-align: center;
+    font-size: 30px;
+    font-family: "KOTRA_BOLD";
+    color: #4659A9;
+    position: absolute;
+    left: 230px;
+    bottom: 340px;
+`;
+
+const DrawChart22 = styled(DrawChart2)`
+`;
+
+const Container2 = styled.div`
+    width: 540px;
+    height: 323px;
+    border-radius: 61px;
+    background: #97A4E8;
+    opacity: 0.3;
+    display: inline-block;
+    margin-bottom: 50px;
+`;
+
+const Review = styled.div`
+    grid-area: review;
+    position: relative;
+`;
+
+const SubtitleText3 = styled.p`
+    text-align: center;
+    font-size: 30px;
+    font-family: "KOTRA_BOLD";
+    color: #4659A9;
+    position: absolute;
+    left: 145px;
+    bottom: 340px;
+`;
+
+const Container3 = styled.div`
+    width: 540px;
+    height: 323px;
+    border-radius: 61px;
+    background: #4659A9;
+    opacity: 0.3;
+    display: inline-block;
+    margin-bottom: 50px;
+    margin-right: 30px;
+`;
+
+const Calendar = styled.div`
+    grid-area: calendar;
+    position: relative;
+`;
+
+const SubtitleText4 = styled.p`
+    text-align: center;
+    font-size: 30px;
+    font-family: "KOTRA_BOLD";
+    color: #4659A9;
+    position: absolute;
+    left: 210px;
+    bottom: 340px;
+`;
+
+const Container4 = styled.div`
+    width: 540px;
+    height: 323px;
+    border-radius: 61px;
+    background: #6F61C6;
+    opacity: 0.3;
+    display: inline-block;
+    margin-bottom: 50px;
+`;
 
 export default StatisticsPage;
