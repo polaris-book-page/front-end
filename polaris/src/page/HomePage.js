5c74a70d54788cc2d821ec584e6e969dd94d8ddf
otcroz <dbtndus0527@gmail.com>
2024-03-03 02:24:29 +0900

    [feat/design] Implement a basic interaction background in Homepage - using gsap

--- polaris/src/page/HomePage.js
+++ polaris/src/page/HomePage.js
@@ -5,12 +5,59 @@ import VerticalFlowText from "../component/VerticalFlowText";
 import { useNavigate } from "react-router-dom";
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from "axios";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from 'gsap/ScrollTrigger'; 
 
 const HomePage = () => {
     // useNavigate
     const navigate = useNavigate();
     const queryClient = useQueryClient()
     const initialData = queryClient.getQueryData(['check']);
+
+    // interaction
+    gsap.registerPlugin(ScrollTrigger);
+    const container = useRef();
+    
+    useGSAP(() => {
+        const boxItems = gsap.context((self) => {
+            const boxes = self.selector('.box');
+            boxes.forEach((box) => {
+                gsap.from(box, {
+                    x: 1000,
+                    y: 0,
+                    stagger: 1,
+                    opacity: 0,
+                    scrollTrigger: {
+                        trigger: box,
+                        scrub: true,
+                        start: "top bottom",
+                        end: "top",
+                        snap: 1,
+                        // markers: true,
+                        toggleActions: "restart none reverse none",
+                    },
+                });
+                gsap.to(box, {
+                    x:0,
+                    y: 50,
+                    opacity: 1,
+                    duration: 3,
+                })
+            })
+        }, container)
+        return () => boxItems.revert(); // clean up
+        // gsap.from(".start-box", {
+        //     y: 0,
+        //     opacity: 0.5,
+        // });
+        // gsap.to(".start-box", {
+        //     y: 50,
+        //     opacity: 1,
+        //     duration: 2,
+        // })
+    }, {scope: container})
 
     const RandomFlowTextFuxc = () => {
     // load random quote array 10 sentence(temporary)
@@ -54,12 +101,10 @@ const HomePage = () => {
     return (
         !tenQuoteQuery.isLoading  && tenQuoteQuery.data &&
         <>
-            <NavBar />
             <Background>
                 {RandomFlowTextFuxc()}
-                <Container>
-                    <div style={{margin: 70}} />
-                    <ContentBox>
+                <Container ref={container}>
+                    <ContentBox className="box">
                         <TextBox>
                             <TitleText color={'white'} size={'24px'}>여행자분들의 책 여행 길잡이,</TitleText>
                             <br />
@@ -68,8 +113,7 @@ const HomePage = () => {
                         <div style={{margin: 20}} />
                         <Logo src={require('../assets/graphic/app-logo.png')} />
                     </ContentBox>
-                    <div style={{margin: 150}} />
-                    <ContentBox>
+                    <ContentBox className="box">
                         <TextBox>
                             <TitleText color={'white'} size={'24px'}>북극성 탐험</TitleText>
                             <br />
@@ -77,8 +121,7 @@ const HomePage = () => {
                         </TextBox>
                         <img src={require('../assets/graphic/home-graphic-1.png')} style={{width: 300}} />
                     </ContentBox>
-                    <div style={{margin: 150}} />
-                    <ContentBox>
+                    <ContentBox className="box">
                         <img src={require('../assets/graphic/home-graphic-2.png')} style={{width: 150}} />
                         <div style={{margin: 20}} />
                         <TextBox style={{textAlign: 'end'}}>
@@ -87,26 +130,24 @@ const HomePage = () => {
                             <TitleText color={'#2C2C60'} size={'16px'} >보여지는 문장 중 마음에 드는 문장을 하나 선택하여 <br />책을 추천받을 수 있습니다. <br />오늘의 한 문장이 <br />여러분의 책 여행의 북극성이 되어줄 것입니다.</TitleText>
                         </TextBox>
                     </ContentBox>
-                    <div style={{margin: 200}} />
-                    <ContentBox>
-                        <TitleText color={'white'} size={'20px'} >다양한 책을 여행하면서 <br />여러분의 우주를 확장해보아요.</TitleText>
-                        <div style={{margin: 10}} />
-                        <img src={require('../assets/graphic/planet-4.png')} style={{width: 80}} />
+                    <ContentBox className="box">
+                        <ContentSubBox>
+                            <TitleText color={'white'} size={'20px'} >다양한 책을 여행하면서 <br />여러분의 우주를 확장해보아요.</TitleText>
+                            <div style={{margin: 10}} />
+                            <img src={require('../assets/graphic/home-graphic-3.png')} style={{width: 80}} />
+                        </ContentSubBox>
+                        <ContentSubBox>
+                            <img src={require('../assets/graphic/planet-4.png')} style={{ width: 80 }} />
+                            <div style={{margin: 40}} />
+                            <TitleText color={'white'} size={'20px'}>북극성이 여러분의 여행을 위한<br />길잡이가 되어줄 거예요.</TitleText>
+                        </ContentSubBox>
                     </ContentBox>
-                    <br />
-                    <ContentBox>
-                        <img src={require('../assets/graphic/home-graphic-3.png')} style={{width: 80}} />
-                        <div style={{margin: 40}} />
-                        <TitleText color={'white'} size={'20px'}>북극성이 여러분의 여행을 위한<br />길잡이가 되어줄 거예요.</TitleText>
-                    </ContentBox>
-                    <div style={{margin: 50}} />
                     <Button onClick={() => handleLogin()}>
                         북극성 시작하기
                     </Button>
                     <div style={{margin: 50}} />
                 </Container>
             </Background>
-            <FooterBar />
         </>
     )
 }
@@ -129,6 +170,7 @@ const Background = styled.div`
     background: linear-gradient(#6F61C6, #667BD4, #97A4E8, #C9C6F7, #FACECB);
     padding-left: 3vw;
     padding-right: 3vw;
+    overflow: hidden;
 `;
 
 const Container = styled.div`
@@ -139,7 +181,14 @@ const Container = styled.div`
 
 // box
 const ContentBox = styled.div`
+    height: 100vh;
     display: flex;
+    align-items: center;
+`;
+
+const ContentSubBox = styled.div`
+    display: flex;
+    flex-direction: column;
     align-items: center;
 `;
 
