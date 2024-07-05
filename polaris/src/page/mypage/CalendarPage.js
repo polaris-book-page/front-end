907ec4189349e051ff05c0b91dea7e2374110910
yxxyn20 <dbtndus0527@gmail.com>
2024-07-05 20:37:53 +0900

    [fix] Fix to not update query issue when user move from calendar page to my review page

--- polaris/src/page/mypage/CalendarPage.js
+++ polaris/src/page/mypage/CalendarPage.js
@@ -4,12 +4,13 @@ import styled from 'styled-components';
 import moment from 'moment/moment';
 import NavBar from "../../component/NavBar";
 import { useNavigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 const CalendarPage = () => {
     const [value, onChange] = useState(new Date());
     let navigate = useNavigate();
+    const queryClient = useQueryClient();
 
     // fetch API
     const fetchReviewList = async () => {
@@ -35,6 +36,11 @@ const CalendarPage = () => {
         return ReviewQuery.data.reviewList.findIndex((x) => moment(x.endDate).format("YYYY-MM-DD") === moment(date).format("YYYY-MM-DD"))
     }
 
+    const handleOnClick = async (date) => {
+        await queryClient.invalidateQueries(['detail-review']);
+        navigate(`/mypage/review/detail`, { state: ReviewQuery.data.reviewList[CalIndexFunc(date)] }) 
+    }
+
     return (
         !ReviewQuery.isLoading && ReviewQuery.data &&
         <>
@@ -53,9 +59,7 @@ const CalendarPage = () => {
                         tileContent={({ date }) => {
                             if (ReviewQuery.data.findMyReview && ReviewQuery.data.reviewList.find((x) => moment(x.endDate).format("YYYY-MM-DD") === moment(date).format("YYYY-MM-DD"))) {
                                 return (
-                                    <>
-                                        <BookImage onClick={()=> navigate(`/mypage/review/detail`, { state: ReviewQuery.data.reviewList[CalIndexFunc(date)] })} src={ReviewQuery.data.reviewList[CalIndexFunc(date)]['bookImage']} />
-                                    </>
+                                    <BookImage onClick={() => handleOnClick(date)} src={ReviewQuery.data.reviewList[CalIndexFunc(date)]['bookImage']} />
                                 )
                             }
                             }
