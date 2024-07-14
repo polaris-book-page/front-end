c8d3dd864852973105d95d5ded17d53cb430be07
kjh073 <9279482@naver.com>
2024-07-15 04:13:29 +0900

    [style] When quote is longer than box, apply scroll - when quote is longer than box, apply scroll

--- polaris/src/component/EachSentence.js
+++ polaris/src/component/EachSentence.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { PiBookFill } from "react-icons/pi";
 
 const EachSentence = ({ onClick, quote, bookCategory, bookColor }) => {
+    const isShortText = quote.length < 110
+    
     return (
         <Container onClick={onClick}>
             <QuoteBox>
-            <QuoteText>{quote}</QuoteText>
+                <QuoteText isShortText={isShortText}>{quote}</QuoteText>
             </QuoteBox>
             <BookIcon size="200" color={bookColor} />
             <BookCategory>{bookCategory}</BookCategory>
@@ -27,20 +29,27 @@ const QuoteBox = styled.div`
     position: relative;
     align-items: center;
     display: flex;
-    min-height: 130px;
-    width: 450px;
+    height: 130px;
+    width: 530px;
     background-image: linear-gradient(to bottom, white, rgba(217, 217, 217, 0.5));
     border: 2px solid white;
     clip-path: polygon(0% 0%, 100% 0, 88% 50%, 100% 100%, 0% 100%);
-`;
+    `;
 
 const QuoteText = styled.div`
-    width: 300px;
+    vertical-align: middle;
+    overflow-y: scroll;
+    height: 110px;
+    width: 340px;
     margin-left: 130px;
     color: black;
     font-size: 16px;
     font-family: "KOTRA_GOTHIC";
-    padding-right: 40px;
+    padding-right: 20px;
+    ${props => props.isShortText && css`
+        display: flex;
+        align-items: center; 
+    `}
 `;
 
 const BookIcon = styled(PiBookFill)`
