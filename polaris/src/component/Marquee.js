60b414c13296508ba9b9b48e8f84d012202cb158
kjh073 <9279482@naver.com>
2024-07-16 20:52:50 +0900

    [design] Modify css of marquee - modify css of marquee

--- polaris/src/component/Marquee.js
+++ polaris/src/component/Marquee.js
@@ -11,27 +11,15 @@ const textLoop = keyframes`
 `;
 
 const FlowContainer = styled.div`
-	width: 145px;
-	margin-right: 10px;
 	overflow: hidden;
-	@media screen and (min-width: 516px) {
-        width: 152px;
-    }
-    @media screen and (max-width: 515px) {
-        width: 150px;
-    }
-	@media screen and (max-width: 380px) {
-        width: 90px;
-    }
+	margin-right: 10px;
 `;
 
 const FlowWrap = styled.div`
-	width: 145px;
-	line-height: 1.5;
 	font-size: ${(props) => props.size || '19px'};
 	color: ${(props) => props.color || '#4659A9' };
 	font-weight: 700;
-	font-family: "KOTRA_GOTHIC";
+	font-family: ${(props) => props.font || "KOTRA_GOTHIC"};
 	&:hover {
 		animation: ${textLoop} 6.5s linear infinite;
 	}
@@ -52,11 +40,11 @@ const FlowText = styled.div`
 	white-space: nowrap;
 `;
 
-const Marquee = ({title, color, size}) => {
+const Marquee = ({title, color, size, font}) => {
 	return (
 		<FlowContainer>
 			<FlowText>
-				<FlowWrap color={color} size={size}>{ title }</FlowWrap>
+				<FlowWrap color={color} size={size} font={font}>{ title }</FlowWrap>
 			</FlowText>
 		</FlowContainer>
 	);
